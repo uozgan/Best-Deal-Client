4d21c54418baed39949a9703a410cbcf0225dabe
uozgan <utkuozgan_8@hotmail.com>
2020-04-23 17:33:16 +0200

    cart Products fetched and logged

--- src/pages/MyDeals/index.js
+++ src/pages/MyDeals/index.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Loading from "../../components/Loading";
 import { Container } from "react-bootstrap";
+import { selectCart } from "../../store/user/selectors";
+import { fetchCartProductsByCartId } from "../../store/user/actions";
 
 export default function MyDeals() {
-  const cart = [];
+  const dispatch = useDispatch();
+  const cart = useSelector(selectCart);
+  //const cartId = cart.id;
+
+  const cartId = cart ? cart.id : null;
+
+  useEffect(() => {
+    dispatch(fetchCartProductsByCartId(cartId));
+  }, []);
 
   if (!cart) {
     return <Loading />;
   }
 
-  return <Container>My Deals</Container>;
+  console.log("Cart", cart);
+
+  return <Container>My Deals Id: {cart.id}</Container>;
 }
